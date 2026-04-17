866469f7f450cd131360f2dfd5af6b3c8fd5d8d0
Sunderiya Buyannemekh <S>
2026-04-17 22:42:39 +1000

    CUS2 added + CUS1 limit setting fix

--- search.py
+++ search.py
@@ -1,6 +1,7 @@
 import sys
 import math
 import heapq
+import random
 from collections import deque
 
 
@@ -301,13 +302,145 @@ def depth_limited_search(origin, destinations, edges, limit):
 
     result = recursive_dls(root, {origin}, limit)
 
-    if result is CUTOFF or result is None:
+    if result is CUTOFF:
+        return "CUTOFF", nodes_created
+    if result is None:
         return None, nodes_created
-
     return result, nodes_created
+
+
+def ALT_hn(state,destinations,Landmarks,LM_Table):    
+    goals = []
+    # to check for any of the valid destinations
+    for goal in destinations:
+        best_distance = float("-inf")
+        for L in Landmarks:
+            dist = abs(LM_Table[L][state] - LM_Table[L][goal])
+            best_distance = max(best_distance,dist)
+        goals.append(best_distance)
+            
+        
+    #return best distance
+    return min(goals)
+    
+def Landmark_table(landmarks, edges):
+    LM_Table = {}
+
+    for L in landmarks:
+        #value here is node to Landmark
+        LM_Table[L] = (Dijkstra_Calc(edges, L))
+    return LM_Table
+
+    
+def Dijkstra_Calc(edges, landmark):
+    #same as A* but with h(n) = 0 and returns shortest distance instead of path to goal node
+    nodes_created = 1
+    counter = 0
+
+    root = Node(landmark, None, 0, 0, counter)
+        # (f_cost, node_id, insertion_counter, node)
+    frontier = [(root.path_cost, root.state, counter, root)]
+    best_g = {landmark: 0}
+    landmark_dist = {}
+    while frontier:
+        _, _, _, current = heapq.heappop(frontier)
+
+        if current.path_cost > best_g.get(current.state, float("inf")):
+            continue
+        #saves distance to landmark for the current state into a dictionary
+        landmark_dist[current.state] = current.path_cost
+        
+        neighbours = edges.get(current.state, [])
+
+        for next_state, edge_cost in neighbours:
+            new_g = current.path_cost + edge_cost
+
+            if new_g < best_g.get(next_state, float("inf")):
+                best_g[next_state] = new_g
+                counter += 1
+
+                child = Node(
+                    next_state,
+                    current,
+                    new_g,
+                    current.depth + 1,
+                    counter
+                )
+                nodes_created += 1
+
+                f = new_g
+
+                heapq.heappush(frontier, (f, next_state, child.created_order, child))
+    return landmark_dist
+
+
+def a_landmark_triangle_inequality_search(nodes, edges, origin, destinations):
+    #same as A* but using different h(n)
+    nodes_created = 1
+    counter = 0
+
+    root = Node(origin, None, 0, 0, counter)
+
+    #create random landmarks based on given nodes with a minimum of 2 landmarks and a max of 16
+    k = min(16, max(2, int(len(nodes) ** 0.5)))
+    k = min(k, len(nodes))
+    landmarks = random.sample(list(nodes.keys()), k)
+
+    if root.state in destinations:
+        return root, nodes_created
+    
+    #creates a dictionary contain all distances for all nodes from all landmarks
+    landmark_table = Landmark_table(landmarks, edges)
+    root_h = ALT_hn(origin,destinations,landmarks,landmark_table)
+    
+    # (f_cost, node_id, insertion_counter, node)
+    frontier = [(root.path_cost + root_h, root.state, counter, root)]
+    best_g = {origin: 0}
+
+    while frontier:
+        _, _, _, current = heapq.heappop(frontier)
+
+        if current.path_cost > best_g.get(current.state, float("inf")):
+            continue
+
+        if current.state in destinations:
+            return current, nodes_created
+        
+        neighbours = edges.get(current.state, [])
+
+        for next_state, edge_cost in neighbours:
+            new_g = current.path_cost + edge_cost
+
+            if new_g < best_g.get(next_state, float("inf")):
+                best_g[next_state] = new_g
+                counter += 1
+
+                child = Node(
+                    next_state,
+                    current,
+                    new_g,
+                    current.depth + 1,
+                    counter
+                )
+                nodes_created += 1
+ 
+                h = ALT_hn(next_state,destinations,landmarks,landmark_table)
+                f = new_g + h
+
+                heapq.heappush(frontier, (f, next_state, child.created_order, child))
+
+    return None, nodes_created
+
+
 
 def print_result(filename, method, result_node, nodes_created):
     print(filename, method)
+
+    #For DLS Cutoff result 
+    if result_node == "CUTOFF":
+        print("CUTOFF", nodes_created)
+        print("DLS: Depth limit reached")
+        return
 
     if result_node is None:
         print("None", nodes_created)
@@ -320,8 +453,8 @@ def print_result(filename, method, result_node, nodes_created):
 
 
 def main():
-    if len(sys.argv) != 3:
-        print("Usage: python search.py <filename> <method>")
+    if len(sys.argv) < 3:
+        print("Usage: python search.py <filename> <method> [limit]")
         sys.exit()
 
     filename = sys.argv[1]
@@ -337,10 +470,17 @@ def main():
         result_node, nodes_created = greedy_best_first_search(nodes, edges, origin, destinations)
     elif  method == "AS":
         result_node, nodes_created = astar_search(nodes, edges, origin, destinations)
-    elif  method == "DLS":
-        result_node, nodes_created = depth_limited_search(origin, destinations, edges, limit=50)
+    elif method == "DLS":
+        if len(sys.argv) < 4:
+            print("For DLS specify limit: python search.py <filename> DLS <limit>")
+            sys.exit()
+
+        limit = int(sys.argv[3])
+        result_node, nodes_created = depth_limited_search(origin, destinations, edges, limit)
+    elif  method == "AL":
+        result_node, nodes_created = a_landmark_triangle_inequality_search(nodes, edges, origin, destinations)
     else:
-        print("Please choose from BFS, DFS, GBFS, AS, DLS, CUS2")
+        print("Please choose from BFS, DFS, GBFS, AS, DLS <limit>, ALT")
         return
 
     print_result(filename, method, result_node, nodes_created)
