44d22d08377d38ee06f5e7b21c029d2706386ed4
Steve Shandran Henry Albert <steveshandran@gmail.com>
2026-05-10 01:26:42 +1000

    Gru model update

---
+++ PartB/GRU Model/train.py
@@ -0,0 +1,69 @@
+"""
+Train the NN model.
+"""
+import sys
+import os
+import warnings
+import argparse
+import numpy as np
+import pandas as pd
+from data import process_data
+from model import model
+from keras.models import Model
+from keras.callbacks import EarlyStopping
+warnings.filterwarnings("ignore")
+
+
+def train_model(model, X_train, y_train, name, config, scat, location):
+    """train
+    train a single model.
+
+    # Arguments
+        model: Model, NN model to train.
+        X_train: ndarray(number, lags), Input data for train.
+        y_train: ndarray(number, ), result data for train.
+        name: String, name of model.
+        config: Dict, parameter for train.
+    """
+
+    model.compile(loss="mse", optimizer="rmsprop", metrics=['mape'])
+    # early = EarlyStopping(monitor='val_loss', patience=30, verbose=0, mode='auto')
+    hist = model.fit(
+        X_train, y_train,
+        batch_size=config["batch"],
+        epochs=config["epochs"],
+        validation_split=0.05)
+
+    model.save('model/' + scat + "-" + location + "-" + name + '.h5')
+    df = pd.DataFrame.from_dict(hist.history)
+    df.to_csv('model/' + name + ' loss.csv', encoding='utf-8', index=False)
+
+
+
+
+def main(argv):
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "--model",
+        default="gru",
+        help="Model to train.")
+    args = parser.parse_args()
+
+    lag = 12
+    config = {"batch": 256, "epochs": 600}
+    for scat in os.listdir("../SCATS_Data"):
+        for location in os.listdir(os.path.join("../SCATS_Data", scat)):
+            file1 = os.path.join("../SCATS_Data", scat, location, "train.csv")
+            file2 = os.path.join("../SCATS_Data", scat, location, "test.csv")
+            X_train, y_train, _, _, _, _, _ = process_data(file1, file2, lag)
+
+
+            if args.model == 'gru':
+                X_train = np.reshape(X_train, (X_train.shape[0], X_train.shape[1], 1))
+                m = model.get_gru([12, 64, 64, 1])
+                train_model(m, X_train, y_train, args.model, config, scat, location)
+
+
+
+if __name__ == '__main__':
+    main(sys.argv)
